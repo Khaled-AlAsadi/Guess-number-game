03851d35f9842d5b7588e6215abae15808d8970e
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-01-10 14:08:46 +0100

    Update and changes to the rules

--- game.py
+++ game.py
@@ -2,75 +2,101 @@ from random import randint
 import validation
 import gspread
 from google.oauth2.service_account import Credentials
+from colorama import init, Fore, Back, Style
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-CREDS = Credentials.from_service_account_file('creds.json')
+    "https://www.googleapis.com/auth/drive",
+]
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Guess-Number-Game')
-Blad1 = SHEET.worksheet('Blad1')
-data = Blad1.get_all_values()
-
-print(data)
-
+SHEET = GSPREAD_CLIENT.open("Guess-Number-Game")
+Blad1 = SHEET.worksheet("Blad1")
 
 choice = ""
 level = 1
 max_number = 10
+name = ""
+score = 0
+
+
 def rules():
-  print(
-    "Rule 1 : No\n"
-    "Rule 2 : Yes\n"
-  )
-  global choice
-  choice = input("Return to menu by typing 0 and press enter\n")
-  if not validation.checkChoice(choice):
-     print("please enter a valid choice")
-  elif int(choice) == 0:
-     menu()
+    """
+    Function that displays the rules of the game
+    """
+    validation.clear_console()
+    print(
+        "Rule 1 : The player guess the number\n"
+        "Rule 2 : On each level the max number for the guess increaases\n"
+        "Rule 3 : On level completion the player gets 50 points\n"
+        "Rule 4 : The result gets saved to google spreadsheets on the player chooses not to continue"
+    )
+    global choice
+    choice = input("Return to menu by typing 0 and press enter\n")
+    if not validation.checkChoice(choice):
+        print("please enter a valid choice")
+    elif int(choice) == 0:
+        validation.clear_console()
+        menu()
+
 
 def menu():
-  print(
-  "1. Start Game\n"
-  "2. Rules"
-  )
-  choice = input(
-    """Please choose an option from the menu and type the number of the choice and press enter\n""")  
-  while not validation.checkChoice(choice):
-        choice = input("Please choose a valid option from the menu and type the number of the choice\n")
-  if int(choice) == 1:
-     startGame(level,max_number)
-  if int(choice) == 2:
-     rules()
+    """
+    Main Menu function that runs first
+    """
+    print(Style.BRIGHT + "1. Start Game\n" "2. Rules")
+    choice = input(
+        "Please choose an option from the menu and type the number of the choice and press enter\n"
+    )
+    while not validation.checkChoice(choice):
+        choice = input(
+            Fore.WHITE
+            + Back.RED
+            + "Please choose a valid option from the menu and type the number of the choice\n"
+        )
+    if int(choice) == 1:
+        validation.clear_console()
+        startGame(level, max_number)
+    if int(choice) == 2:
+        rules()
 
-def startGame(level,max_number):
-   validation.clear_console()
-   print("Level:" , level) 
-   random_number = randint(1, max_number)
-   print("Guess a number between 1 and " + str(max_number))
-   guess = int(input("Enter your guess:\n "))
-   while random_number != guess:
+
+def startGame(level, max_number):
+    """
+    Function that handles the game logic
+    """
+    global score
+    global name
+    name = input("Please type your name and press enter\n") if level == 1 else name
+    validation.clear_console()
+    print("Level:", level)
+    print("Score:", score)
+    random_number = randint(1, max_number)
+    print("Guess a number between 1 and " + str(max_number))
+    guess = int(input("Enter your guess:\n "))
+    while random_number != guess:
         if guess < random_number:
             print("low number")
             guess = int(input("Enter your guess again:\n "))
         elif guess > random_number:
             print("Too high number")
             guess = int(input("Enter your guess again:\n "))
-   print("Congrats you guessed the number. The number is " + str(random_number))
+    print("Congrats you guessed the number. The number is " + str(random_number))
+    score += 50
 
+    if validation.check_input():
+        max_number += 5
+        print(max_number)
+        level += 1
+        startGame(level, max_number)
+        print(max_number)
+    else:
+        NEW_DATA = [name, level, score]
+        Blad1.append_row(NEW_DATA)
+        validation.clear_console()
+        menu()
 
-   if validation.check_input():
-       max_number += 5
-       print(max_number)
-       level += 1
-       startGame(level,max_number)
-       print(max_number)
-   else:
-       validation.clear_console()
-       menu()
 
 menu()
--- validation.py
+++ validation.py
@@ -1,20 +1,13 @@
 import os
 import time
 from colorama import init, Fore, Back, Style
-"""
-# Initialize colorama
-init(autoreset=True)
 
-# Example text with color
-print(Fore.RED + 'This is red text')
-print(Back.GREEN + 'This has a green background')
-print(Style.BRIGHT + 'This is bright text')
-print(Fore.CYAN + Back.YELLOW + 'Cyan text on yellow background')
-"""
 def checkChoice(text):
     try:
         if int(text) < 0:
-            print("Please enter a positive number.")
+            print(Fore.WHITE + Back.RED + "Please enter a positive number.")
+            return False
+        elif int(text) > 2:
             return False
         else:
             return True
