7b5cd28b7cb6bf1a4bbbd6e156a3d26d5965793d
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-01-16 17:34:14 +0100

    Another try

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import sys
 import game
+
 def main():
-    if sys.stdin.isatty():
+    if len(sys.argv) > 1 and sys.argv[1] == "--run-game":
         input("Hit enter to start the program...")
         game.menu()
+    else:
+        print("Game not started. Use '--run-game' as a command-line argument to start the game.")
 
 if __name__ == "__main__":
     main()
