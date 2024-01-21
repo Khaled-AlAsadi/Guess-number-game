cb26b9d722fea08eda040971dc2ae6efc3377f40
Khaled AL Asadi <khelo.asadi@gmail.com>
2024-01-21 10:11:06 +0100

    Reformat validation.py

--- validation.py
+++ validation.py
@@ -2,22 +2,30 @@ import os
 import time
 from colorama import Fore, Back, Style
 
+
 def checkChoice(text):
     try:
         if int(text) < 0:
-            print(Fore.WHITE + Back.RED + "Please enter a positive number.")
+            print(
+                Fore.WHITE
+                + Back.RED
+                + "Please enter a positive number."
+                + Style.RESET_ALL
+            )
             return False
         elif int(text) > 2:
             return False
         else:
             return True
     except ValueError:
-        print("Please enter a valid number.")
+        print(Fore.RED + "Please enter a valid number." + Style.RESET_ALL)
         return False
+
 
 def check_input():
     while True:
-        answer = input("Do you want to proceed to the next level? (yes/no): \n").lower()
+        answer = input(
+            "Do you want to proceed to the next level? (yes/no): \n").lower()
         if answer == "yes":
             print(Style.RESET_ALL)
             return True
@@ -26,15 +34,20 @@ def check_input():
             return False
         else:
             print(Fore.RED + "Invalid input. Please enter 'yes' or 'no'.")
+            print(Style.RESET_ALL)
             time.sleep(0.1)
+
+
 def check_name(name):
     if len(name.strip()) == 0:
-        print(Fore.RED + "Please enter a valid name")
+        print(Fore.RED + "Please enter a valid name" + Style.RESET_ALL)
         return True
     else:
         return False
+
+
 def clear_console():
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
